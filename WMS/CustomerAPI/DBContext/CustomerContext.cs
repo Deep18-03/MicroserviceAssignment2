c3f4ef4acce13f485b02c4b458de45ec0ca41058
Deep18-03 <62807305+Deep18-03@users.noreply.github.com>
2021-02-06 17:03:27 +0530

    Added Microservice Assignment 2

---
+++ WMS/CustomerAPI/DBContext/CustomerContext.cs
@@ -0,0 +1,16 @@
+﻿using CustomerAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAPI.DBContext
+{
+    public class CustomerContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnmodelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ WMS/CustomerAPI/Entities/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Entities
+{
+    public class Location
+    {
+        public int id { get; set; }
+        public string Location { get; set; }
+    }
+}
